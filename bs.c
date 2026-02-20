e2a8a312de49461fcd644f9e7e1fedc186864c72
benzenergy <161916636+benzenergy@users.noreply.github.com>
2026-02-20 13:07:40 +0400

    Create bs.c

---
+++ bs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14159
+
+void myFunction();
+int main() {
+    int age = 25;
+    printf("Hello, World!\n");
+    myFunction();
+    return 0;
+}
+void myFunction() {
+
+}
